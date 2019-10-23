3dae9980836326b6cf724da5a9d4c7c28b3f85e6
Gabriel Desjardins <gdesjard@teksystems.com>
2019-10-23 17:57:59 -0400

    virtual scrolling themed resizable community edition  = MIT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import { AgGridReact } from 'ag-grid-react';
 
 function App() {
+    const [columnDefs, setColumnDefs] = useState([{
+        headerName: "Make", field: "make", resizable: true
+    }, {
+        headerName: "Model", field: "model", resizable: true
+    }, {
+        headerName: "Price", field: "price", resizable: true
+    }]);
+
+    const [rowData, setRowData] = useState([{
+        make: "Toyota", model: "Celica", price: 35000
+    }, {
+        make: "Ford", model: "Mondeo", price: 32000
+    }, {
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Toyota", model: "Celica", price: 35000
+    }, {
+        make: "Ford", model: "Mondeo", price: 32000
+    }, {
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Toyota", model: "Celica", price: 35000
+    }, {
+        make: "Ford", model: "Mondeo", price: 32000
+    }, {
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Toyota", model: "Celica", price: 35000
+    }, {
+        make: "Ford", model: "Mondeo", price: 32000
+    }, {
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    },{
+        make: "Porsche", model: "Boxter", price: 72000
+    }]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+            className="ag-theme-material"
+            style={{
+                height: '500px',
+                width: '600px' }}
         >
-          Learn React
-        </a>
-      </header>
+            <AgGridReact
+                columnDefs={columnDefs}
+                rowData={rowData}>
+            </AgGridReact>
+        </div>
     </div>
   );
 }
